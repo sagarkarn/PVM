36d68791062b6adc348c13f4616e989c8fb1a8e3
Sagar Karn <sagarkarn9@gmail.com>
2024-04-07 12:18:13 +0530

    Refactor ExtEnableCommand.cs to improve extension enabling logic

--- PVM/Commands/ExtEnableCommand.cs
+++ PVM/Commands/ExtEnableCommand.cs
@@ -11,20 +11,11 @@ namespace PVM.Commands
     {
         public void ExtEnable([Argument]string ext)
         {
-            var dllName = "php_" + ext + ".dll";
-            var extPath = Path.Join(Directory.GetCurrentDirectory(), "php", "ext");
-            if (!Directory.Exists(extPath))
+            if (!DllInExtDir(ext))
             {
-                Console.WriteLine("ext not found");
                 return;
             }
-
-            var extDll = Path.Join(extPath, dllName);
-            if (!File.Exists(extDll))
-            {
-                Console.WriteLine("file not found");
-                return;
-            }
+            
 
             var phpIniPath = Path.Join(Directory.GetCurrentDirectory(), "php", "php.ini");
             if (!File.Exists(phpIniPath))
@@ -46,11 +37,36 @@ namespace PVM.Commands
                 Console.WriteLine("extension already enabled");
                 return;
             }
-
-            lines.Add("extension=" + ext);
-            File.WriteAllLines(phpIniPath, lines);
-
-            Console.WriteLine("extension enabled");
+            extLine = lines.FirstOrDefault(l => l.StartsWith(";extension=" + ext));
+            if (extLine == null)
+            {
+                Console.WriteLine("extension not found in ini file");
+            }
+            else
+            {
+                var index = lines.IndexOf(extLine);
+                lines[index] = "extension=" + ext;
+                File.WriteAllLines(phpIniPath, lines);
+                Console.WriteLine("extension enabled");
+            }
+        }
+        [Ignore]
+        private static bool DllInExtDir(string ext)
+        {
+            var dllName = "php_" + ext + ".dll";
+            var extPath = Path.Join(Directory.GetCurrentDirectory(), "php", "ext");
+            if (!Directory.Exists(extPath))
+            {
+                Console.WriteLine("ext not found");
+                return false;
+            }
+            var extDll = Path.Join(extPath, dllName);
+            if (!File.Exists(extDll))
+            {
+                Console.WriteLine("file not found");
+                return false;
+            }
+            return true;
         }
     }
 }
